81df806a9a63358e3a9577eafb6c60278166ad0f
Karen Javadyan <kajarenc@gmail.com>
2025-03-13 19:28:42 +0400

    use latest wheel file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 st.write(st.context.timezone)
 st.write(st.context.timezone_offset)
 st.write(st.context.locale)
+st.write("---------------------------------")
 st.write(st.context.url)
 
 
